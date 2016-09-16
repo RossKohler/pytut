f33348c7481d1729d5114e4f15f90f055b0f4785
carlakirkcohen <carlakirkcohen@gmail.com>
2016-09-16 10:45:26 +0200

    can't debug with syntax errors

--- app/scripts/controllers/interpreter.js
+++ app/scripts/controllers/interpreter.js
@@ -23,6 +23,7 @@ angular.module('yapp')
        //var placeHere = document.getElementById("frameHere");
        //placeHere.innerHTML="";
      };
+
       function outf(text) {
           var mypre = document.getElementById("output");
           mypre.innerHTML = mypre.innerHTML + text;
@@ -35,18 +36,36 @@ angular.module('yapp')
       }
 
       $scope.debugit = function() {
-        clearAll();
-        document.getElementById("debHd").style.visibility="visible";
-        var placeHere = document.getElementById("frameHere");
-        var iframe = document.createElement('iframe');
-        iframe.style.display = "inline";
-        iframe.id ="debugger";
-        iframe.width="90%";
-        iframe.height="600px";
-        var url = "http://pythontutor.com/iframe-embed.html#code=" + encodeURIComponent(aceHl.getValue())+ "&codeDivHeight=400&codeDivWidth=550&cumulative=false&curInstr=0&heapPrimitives=false&origin=opt-frontend.js&py=2&rawInputLstJSON=%5B%5D&textReferences=false";
-        iframe.src = url;
-        placeHere.appendChild(iframe);
+        // must run program to check that there are no syntax errors before debugging
+        var prog = aceHl.getValue();
+        var mypre = document.getElementById("output");
 
+        mypre.innerHTML = '';
+        Sk.pre = "output";
+        Sk.configure({ output: outf, read: builtinRead });
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+        var myPromise = Sk.misceval.asyncToPromise(function () {
+            return Sk.importMainWithBody("<stdin>", false, prog, true);
+        });
+
+        myPromise.then(function (mod) {
+          clearAll();
+          document.getElementById("debHd").style.visibility="visible";
+          var placeHere = document.getElementById("frameHere");
+          var iframe = document.createElement('iframe');
+          iframe.style.display = "inline";
+          iframe.id ="debugger";
+          iframe.width="90%";
+          iframe.height="600px";
+          var url = "http://pythontutor.com/iframe-embed.html#code=" + encodeURIComponent(aceHl.getValue())+ "&codeDivHeight=400&codeDivWidth=550&cumulative=false&curInstr=0&heapPrimitives=false&origin=opt-frontend.js&py=2&rawInputLstJSON=%5B%5D&textReferences=false";
+          iframe.src = url;
+          placeHere.appendChild(iframe);
+        },
+            function (err) {
+              var mypre = document.getElementById("output");
+              mypre.innerHTML = "Cannot debug with syntax errors: \n" +mypre.innerHTML + err.toString();
+            });
+          console.log("debugging");
       }
 
       $scope.runit = function() {
